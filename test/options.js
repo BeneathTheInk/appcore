957fa8e336afd6194e07fa6bc2dfa117ec0ac44d
Tyler Johnson <tyler@vintyge.com>
2015-07-30 13:54:33 -0600

    added more tests with bug fixes

---
+++ test/options.js
@@ -0,0 +1,94 @@
+var _ = require("underscore");
+var Appcore = require("../");
+var test = require("tape");
+
+test("set() sets root value", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	var value = { foo: "bar" };
+	app.set(value);
+	t.deepEqual(app.options, value, "root value contains the value set");
+});
+
+test("set() sets value at key", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	var value = { bar: "baz" };
+	app.set("foo", value);
+	t.deepEqual(app.options.foo, value, "'foo' value contains the value set");
+});
+
+test("set() merges plain objects", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	app.set({ foo: { bar: "baz" } });
+	app.set("foo", { hello: "world" });
+	t.deepEqual(app.options.foo, { bar: "baz", hello: "world" }, "'foo' value contains the merged value");
+});
+
+test("set() replaces non-plain objects", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	var value = { hello: "world" };
+	app.set({ foo: "bar" });
+	app.set("foo", value);
+	t.deepEqual(app.options.foo, value, "'foo' value contains the replaced value");
+});
+
+test("reset() replaces root value", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	var value = { foo: { bar: "baz" } };
+	app.set({ hello: "world" });
+	app.reset(value);
+	t.equal(app.options, value, "root value is the reset value");
+});
+
+test("reset() replaces value at key", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	var value = { bar: "baz" };
+	app.set("foo", { hello: "world" });
+	app.reset("foo", value);
+	t.equal(app.options.foo, value, "'foo' value is the reset value");
+});
+
+test("defaults() sets over undefined, ignores real values", function(t) {
+	t.plan(2);
+	var app = Appcore();
+	app.set({ hello: "world" });
+	app.defaults({ hello: true, foo: "bar" });
+	t.equal(app.options.foo, "bar", "set over undefined");
+	t.equal(app.options.hello, "world", "ignored real value");
+});
+
+test("unset() sets root value to an object", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	app.set({ hello: "world" });
+	app.unset();
+	t.deepEqual(app.options, {}, "unset the root value");
+});
+
+test("unset() sets value at key to undefined", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	app.set({ foo: "bar" });
+	app.unset("foo");
+	t.equal(app.options.foo, void 0, "unset 'foo' value");
+});
+
+test("get() retrieves the root value with defaults", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	app.set({ foo: "bar" });
+	var value = app.get();
+	t.deepEqual(value, _.extend({}, Appcore.defaults, { foo: "bar" }), "value merged with defaults");
+});
+
+test("get() gets value at key", function(t) {
+	t.plan(1);
+	var app = Appcore();
+	app.set({ foo: { bar: "baz" } });
+	t.equal(app.get("foo.bar"), "baz", "gets deep value");
+});
